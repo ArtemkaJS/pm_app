44f504d4e929a483f48e22cb56d532b0b3f7246b
ArtemkaJS <artemkajs@mail.ru>
2015-02-25 16:43:04 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pm::Application.config.secret_token = '78035385a8e2e73ab4a8d394e4fea2dcc0d3b34c797c94d097b885c6ee8a0c09146469604e54abf246ce2688b8cd84d2dc8ef4f61e09e564150c16f714c3e2da'
